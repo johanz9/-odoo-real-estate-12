7137897dd0a0e49d8d9a7da91ab84a182508d87c
johanz9 <johanz147@gmail.com>
2022-03-22 10:01:38 +0100

    added cancel_appointment button

--- extra-addons/tattoo/models/tattoo_appointment.py
+++ extra-addons/tattoo/models/tattoo_appointment.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions
 import datetime
 
 
@@ -17,3 +17,31 @@ class TattooAppointment(models.Model):
     appointment_date = fields.Date(string="Data Appuntamento",
                                    required=True,
                                    default=datetime.datetime.now().date())
+    tattoo_artist_id = client_id = fields.Many2one('res.users', string='Tatuatore',
+                                                   default=lambda self: self.env.uid)
+    client_id = fields.Many2one('res.partner', string='Cliente')
+
+    def cancel_appointment(self):
+        for record in self:
+            record.state = "annullato"
+
+        return True
+
+    @api.model
+    def create(self, vals):
+        # search if the client have a appointment in state "fissato" return a record
+        client_appointment = self.env['tattoo.appointment'].search(
+            [('client_id', '=', vals["client_id"]), ('state', '=', 'fissato')])
+
+        if client_appointment.state == "fissato":
+            raise exceptions.UserError('Il cliente non può avere più di un '
+                                       'appuntamento fissato')
+
+        artist_appointment = self.env['tattoo.appointment'].search(
+            [('tattoo_artist_id', '=', vals["tattoo_artist_id"]), ('state', '=', 'fissato')])
+
+        if artist_appointment.state == "fissato":
+            raise exceptions.UserError('Il tatuatore non può avere più di un '
+                                       'appuntamento fissato')
+
+        return super().create(vals)
