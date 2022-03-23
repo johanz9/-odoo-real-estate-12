af7131e9a40b9457ffa2adae7107404b2bda2d18
johanz9 <johanz147@gmail.com>
2022-03-23 12:13:08 +0100

    updated session

--- extra-addons/tattoo/models/tattoo_session.py
+++ extra-addons/tattoo/models/tattoo_session.py
@@ -20,28 +20,19 @@ class TattooSession(models.Model):
                                    required=True,
                                    default=datetime.datetime.now())
     design_id = fields.Many2one('tattoo.design', string='Disegno')
-    duration = fields.Float(string="Durata Necessaria", compute="_compute_duration")
+    duration = fields.Float(string="Durata Necessaria", related='design_id.time')
     session_cost = fields.Float(string="Costo della sessione", compute="_compute_session_cost")
     design_cost = fields.Float(related='design_id.price')
-    design_time = fields.Float(related='design_id.time')
 
-    @api.depends('design_id')
-    def _compute_duration(self):
-        for record in self:
-            try:
-                design_id = self.env['tattoo.design'].browse(record.design_id.id)
-                record.duration = design_id.time
-            except:
-                pass
 
-    @api.depends('design_cost', 'tattoo_artist_ids', 'design_time')
-    def _compute_duration(self):
+    @api.depends('design_cost', 'tattoo_artist_ids', 'duration')
+    def _compute_session_cost(self):
         for record in self:
             try:
                 # design_id = self.env['tattoo.design'].browse(record.design_id.id)
                 artist_cost = 0
                 for artist in record.tattoo_artist_ids:
-                    artist_cost += artist.hour_cost * record.design_time
+                    artist_cost += artist.hour_cost * record.duration
                 record.session_cost = record.design_cost + artist_cost
             except:
                 pass
