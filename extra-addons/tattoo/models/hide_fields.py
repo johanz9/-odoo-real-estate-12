9da31e4591269f5c867c5087f8a908942f45ca12
johanz9 <johanz147@gmail.com>
2022-03-25 14:35:03 +0100

    updated

--- extra-addons/tattoo/models/hide_fields.py
+++ extra-addons/tattoo/models/hide_fields.py
@@ -1,7 +1,7 @@
 from odoo import fields, models, api
 
 
-class TattooDesign(models.Model):
+class HideTattooDesign(models.Model):
     _inherit = 'tattoo.design'
 
     make_visible = fields.Boolean(string="User", compute='get_user')
@@ -11,7 +11,23 @@ class TattooDesign(models.Model):
         user_crnt = self._uid
 
         res_user = self.env['res.users'].search([('id', '=', self._uid)])
-        if res_user.has_group('tattoo.tattoo_group_manager'):
+        if res_user.has_group('tattoo.tattoo_group_manager') or res_user.has_group('tattoo.tattoo_group_artist'):
+            self.make_visible = True
+        else:
+            self.make_visible = False
+
+
+class HideTattooSession(models.Model):
+    _inherit = 'tattoo.session'
+
+    make_visible = fields.Boolean(string="User", compute='get_user')
+
+    @api.depends('make_visible')
+    def get_user(self, ):
+        user_crnt = self._uid
+
+        res_user = self.env['res.users'].search([('id', '=', self._uid)])
+        if res_user.has_group('tattoo.tattoo_group_manager') or res_user.has_group('tattoo.tattoo_group_artist'):
             self.make_visible = True
         else:
             self.make_visible = False
