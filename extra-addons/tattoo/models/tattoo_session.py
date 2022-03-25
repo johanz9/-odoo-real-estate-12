41f58c86e85894b328cf6b0d68ba6c6f4754be04
johanz9 <johanz147@gmail.com>
2022-03-25 09:04:09 +0100

    added popup message to session

--- extra-addons/tattoo/models/tattoo_session.py
+++ extra-addons/tattoo/models/tattoo_session.py
@@ -54,6 +54,17 @@ class TattooSession(models.Model):
     def finita_session(self):
         for record in self:
             record.state = "finita"
+
+            message_id = self.env['mymodule.message.wizard'].create(
+                {'message': 'Il cambio dello stato della sessione è stato completato con successo'})
+            return {
+                'name': 'Message',
+                'type': 'ir.actions.act_window',
+                'view_mode': 'form',
+                'res_model': 'mymodule.message.wizard',
+                'res_id': message_id.id,
+                'target': 'new'
+            }
         return True
 
     def pagata_session(self):
@@ -69,3 +80,14 @@ class TattooSession(models.Model):
                 (record.id,
                  str(record.client_id.name) + ' - ' + str(record.design_id.name) + ' - ' + str(record.session_date)))
         return res
+
+
+class MyModuleMessageWizard(models.TransientModel):
+    _name = 'mymodule.message.wizard'
+    _description = "Show Message"
+
+    message = fields.Text('Message', required=True)
+
+    @api.multi
+    def action_close(self):
+        return {'type': 'ir.actions.act_window_close'}
